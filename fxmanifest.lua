2381b4c56c85adad12bb7b7954bed1dd856252f6
mP <109157032+ikparu0910@users.noreply.github.com>
2023-10-25 13:45:50 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,10 @@
 shared_script '@mp_var/ai_module_fg-obfuscated.lua'
 shared_script '@mp_var/shared_fg-obfuscated.lua'
 fx_version 'adamant'
-
 game 'gta5'
-
 description 'XP Ranking System'
-
 author 'Karl Saunders'
-
 version '1.2.3'
-
 server_scripts {
     "@oxmysql/lib/MySQL.lua",
     'locales/en.lua',
@@ -18,7 +13,6 @@ server_scripts {
     'utils.lua',
     'server/main.lua'
 }
-
 client_scripts {
     'locales/en.lua',
     'config.lua',
@@ -29,20 +23,14 @@ client_scripts {
     'client/events.lua',
     'demo.lua', -- remove if not required
 }
-
 shared_script {
     '@es_extended/imports.lua',
     '@es_extended/locale.lua',
 }
-
 dependencies {
     'es_extended',
 }
-
-
-
 ui_page 'ui/ui.html'
-
 files {
     'ui/ui.html',
     'ui/fonts/TiltNeon-Regular.ttf',
@@ -52,19 +40,16 @@ files {
     'ui/js/class.leaderboard.js',
     'ui/js/app.js'
 }
-
 export 'ESXP_SetInitial'
 export 'ESXP_Add'
 export 'ESXP_Remove'
 export 'ESXP_SetRank'
-
 export 'ESXP_GetXP'
 export 'ESXP_GetRank'
 export 'ESXP_GetXPToNextRank'
 export 'ESXP_GetXPToRank'
 export 'ESXP_GetMaxXP'
 export 'ESXP_GetMaxRank'
-
 export 'ESXP_ShowUI'
 export 'ESXP_HideUI'
 export 'ESXP_TimeoutUI'
